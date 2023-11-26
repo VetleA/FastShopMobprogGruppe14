cdcd15d020b71f60262b3a9dcf75a43cf1448d76
VetleA <boerstad-96@hotmail.com>
2023-11-26 20:05:59 +0100

    Gjenopprettet handleliste skjerm

--- app/src/main/java/com/example/logerino/handleliste/SpesifikHandlelisteScreen.kt
+++ app/src/main/java/com/example/logerino/handleliste/SpesifikHandlelisteScreen.kt
@@ -1,7 +1,5 @@
 package com.example.logerino.handleliste
 
-import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,48 +8,44 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import kotlinx.coroutines.launch
+import com.example.logerino.navigation.Screens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SpesifikkHandlelisteScreen(
     navController: NavController,
     handlelisteViewModel: HandlelisteViewModel = viewModel(),
-)
-
-{ var tittel by rememberSaveable { mutableStateOf("") }
-    var vare by rememberSaveable { mutableStateOf("") }
+) {
     val getData = handlelisteViewModel.state.value
     val snackbarHostState = remember { SnackbarHostState() }
-    val scope = rememberCoroutineScope()
-    val uiState = handlelisteViewModel.uiState
-    val context = LocalContext.current
-
+    var listSearch by remember { mutableStateOf("") }
+    var ost by remember { mutableStateOf("") }
 
     Scaffold(snackbarHost = { SnackbarHost(snackbarHostState) }) { padding ->
         Column(
@@ -59,22 +53,13 @@ fun SpesifikkHandlelisteScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-
-            OutlinedTextField(
-                modifier = Modifier
-
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                value = tittel,
-                onValueChange = { tittel = it },
-                label = { Text(text = "Tittel") }
+            Text(
+                text = "Mine handlelister",
+                fontWeight = FontWeight.Bold,
+                fontSize = 35.sp
             )
 
-            Button(onClick = {handlelisteViewModel.getDocumentFromFireStore(tittel)
-                Toast.makeText(context, "Handleliste lagt til!", Toast.LENGTH_SHORT).show()}) {
-                Text(text = "Søk etter handleliste")
-            }
-
+            Spacer(modifier = Modifier.size(8.dp))
 
             Card(
                 modifier = Modifier
@@ -85,7 +70,7 @@ fun SpesifikkHandlelisteScreen(
 
                 Text(
                     text = getData.title.toString(),
-                    style = MaterialTheme.typography.headlineLarge,
+                    style = MaterialTheme.typography.headlineMedium,
                     fontWeight = FontWeight.Bold,
                     maxLines = 1,
                     overflow = TextOverflow.Clip,
@@ -100,15 +85,17 @@ fun SpesifikkHandlelisteScreen(
                     modifier = Modifier.padding(4.dp),
                     maxLines = 4
                 )
-
-                Spacer(modifier = Modifier.size(4.dp))
-
             }
-
-
+                Spacer(modifier = Modifier.size(4.dp))
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    TextButton(onClick = { navController.navigate(Screens.HandlelisteScreen.route) }) {
+                        Icon(imageVector = Icons.Default.AddCircle, contentDescription = null)
+                        Text(text = "Opprett ny handleliste")
+                    }
+                }
+            }
         }
-
     }
-
-
-}
